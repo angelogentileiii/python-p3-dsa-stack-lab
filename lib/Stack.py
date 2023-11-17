ceeaeb391d74ce941905c165672d862b08ea7775
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-11-17 09:46:03 -0500

    completed lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        # return not (self.items)
+        return self.items == []
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
-
+        if self.isEmpty():
+            return None
+        else:
+            return self.items.pop()
+        
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
+    # must reverse the stack to count from top down
     def search(self, target):
-        pass
+        reversed_list = list(reversed(self.items))
+        for index in range(len(self.items)- 1, -1, -1):
+            if reversed_list[index] == target:
+                return index
+        return -1
